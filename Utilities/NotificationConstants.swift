9c64c009212df2a9fb8b7b22bdb516728518dce0
Lakshmi <98391736+Lakshmigtech@users.noreply.github.com>
2026-09-15 21:25:15 +0530

    Add NotificationConstants enum for notification keys

---
+++ Utilities/NotificationConstants.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+enum NotificationConstants {
+
+    static let notificationCategory =
+        "GENERAL_NOTIFICATION"
+
+    static let openAction =
+        "OPEN_ACTION"
+
+    static let dismissAction =
+        "DISMISS_ACTION"
+
+    static let notificationTypeKey =
+        "notification_type"
+
+    static let deepLinkKey =
+        "deep_link"
+
+    static let titleKey =
+        "title"
+
+    static let bodyKey =
+        "body"
+
+    static let deviceTokenKey =
+        "device_token"
+}
